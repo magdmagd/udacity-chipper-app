4c3c18cb84a492304b06909bcec56dbe13c752d2
magdmagd <dv.aboelmagd@gmail.com>
2021-01-03 02:06:53 +0200

    Update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+
 
 class App extends Component {
+
+  componentDidMount() 
+  {
+    this.props.dispatch(handleInitialData())
+  }//end componentDidMount() 
+
   render() {
     return (
       <div>
         Starter Code
       </div>
     )
-  }
-}
+  }//end render
+}//end class 
 
-export default App
+//export default App ;
+export default connect()(App)
