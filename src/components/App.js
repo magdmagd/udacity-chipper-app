4c4999406a6a916d38bf1203da464e767d8790f4
magdmagd <dv.aboelmagd@gmail.com>
2021-01-04 15:55:04 +0200

    Adding Components Shows Tweets

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+
 
 
 class App extends Component {
@@ -13,11 +15,21 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code
+       {this.props.loading === true  ? null : <Dashboard />}
       </div>
     )
   }//end render
 }//end class 
 
+
+function mapStateToProps ({ authedUser })
+ {
+  return {
+    loading: authedUser === null
+  }
+}//end mapStateToProps
+
 //export default App ;
-export default connect()(App)
+//export default connect()(App)
+
+export default connect(mapStateToProps)(App) 
---
+++ src/components/Dashboard.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component 
+{
+    render() 
+    {
+      return (
+        <div>
+          <h3 className='center'>Your Timeline</h3>
+          <ul className='dashboard-list'>
+            {this.props.tweetIds.map((id) => (
+              <li key={id}>
+                <div>TWEET ID: {id}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )
+    }//end render 
+  }//end class 
+  
+  function mapStateToProps ({ tweets }) 
+  {
+    return {
+      tweetIds: Object.keys(tweets)
+        .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+  }//end mapStateToProps
+  
+  export default connect(mapStateToProps)(Dashboard) 
