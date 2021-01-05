2f2ce065686eb406590d2a0e6fa49424303f50c8
magdmagd <dv.aboelmagd@gmail.com>
2021-01-05 02:29:12 +0200

    add Tweet Components

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,8 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from './users'
 import { receiveTweets } from './tweets'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 
 const AUTHED_ID = 'tylermcginnis'
 
@@ -9,12 +11,14 @@ export function handleInitialData ()
 {
   return (dispatch) => 
   {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, tweets }) =>
        {
         dispatch(receiveUsers(users))
         dispatch(receiveTweets(tweets))
         dispatch(setAuthedUser(AUTHED_ID))
+        dispatch(hideLoading())
        })
   }//end return (dispatch)
 }//end export 
--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
+import LoadingBar from 'react-redux-loading'
+
 
 
 
@@ -15,7 +17,9 @@ class App extends Component {
   render() {
     return (
       <div>
-       {this.props.loading === true  ? null : <Dashboard />}
+          <LoadingBar />
+                 {this.props.loading === true  ? null : <Dashboard />}
+               
       </div>
     )
   }//end render
